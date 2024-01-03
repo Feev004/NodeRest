08f6e1f7accc9a32c96b6d8931479d51a6bf4bbd
Feev004 <feevfeev2@gmail.com>
2024-01-03 14:04:59 +0700

    up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (rep, res) => {
-  res.send("Hello world! wow Check Nodemon!");
+  res.send("Hello world! wow Check Nodemon!!");
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
