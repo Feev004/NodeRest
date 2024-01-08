cffdd304e6d2012cf2ece1f6ad21d122d619c963
Feev004 <feevfeev2@gmail.com>
2024-01-08 10:25:57 +0700

    up

--- index.js
+++ index.js
@@ -84,6 +84,4 @@ app.delete('/books/:id', (rep, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
